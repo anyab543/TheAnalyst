c06b8876b23aa91252a19df148dc9ab7ad90996d
owennjpr <owenprendergast8@gmail.com>
2023-03-30 22:22:08 -0400

    test scene for the analyst's sixth sense ping ability
    Ping is mostly done, but needs new art and some kind of cooldown

---
+++ The Analyst/Assets/Pulse.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    private Transform pulseTransform;
+    private float rangeCurr;
+    public float rangeMax;
+    private List<Collider2D> alreadyPinged;
+    private bool pulseIsActive;
+
+    void Start() {
+        pulseIsActive = false;
+    }
+
+    private void Awake() {
+        pulseTransform = transform.Find("Pulse_Graphic");
+        rangeMax = 45f;
+        alreadyPinged = new List<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E)) {
+            pulseIsActive = true;
+        }
+        
+        if (pulseIsActive) {
+            float rangeSpeed = 25f;
+            rangeCurr += rangeSpeed * Time.deltaTime;
+
+            if (rangeCurr > rangeMax) {
+                rangeCurr = 0f;
+                alreadyPinged.Clear();
+                pulseIsActive = false;
+            }
+
+            pulseTransform.localScale = new Vector3(rangeCurr, rangeCurr);
+
+            RaycastHit2D[] raycastHit2DArray = Physics2D.CircleCastAll(transform.position, rangeCurr/ 2f, Vector2.zero);
+            foreach (RaycastHit2D raycastHit2D in raycastHit2DArray) {
+                if (raycastHit2D.collider != null) {
+                    if (!alreadyPinged.Contains(raycastHit2D.collider)) {
+                        alreadyPinged.Add(raycastHit2D.collider);
+                    
+                        Transform transformHit = raycastHit2D.transform;
+                        if (transformHit.CompareTag("Hidden")) {
+                            GameObject objectHit = transformHit.GetChild(0).gameObject;
+                            objectHit.SetActive(true);
+                            objectHit.GetComponent<PulseReveal>().setTimer();
+                            Debug.Log("hit hidden object!");
+                        } else {
+                            Debug.Log("hit visibile object!");
+                        }
+                    
+                    }
+                }
+            }
+        
+        }
+
+    }
+}
---
+++ The Analyst/Assets/PulseReveal.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseReveal : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private float disappearTimerCurr;
+    private float disappearTimerMax;
+    private Color color;
+    
+    // Start is called before the first frame update
+    void Start() {
+        //this.SetActive(false);
+    }
+    
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        disappearTimerMax = 1f;
+        disappearTimerCurr = 0f;
+        color = new Color(1, 1, 1, 1f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        disappearTimerCurr += Time.deltaTime / 3;
+
+        color.a = Mathf.Lerp(disappearTimerMax, 0f, disappearTimerCurr / disappearTimerMax);
+        spriteRenderer.color = color;
+
+        if (disappearTimerCurr >= disappearTimerMax) {
+            disappearTimerCurr = 0;
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void setTimer() {
+        disappearTimerCurr = 0;
+    }
+}
