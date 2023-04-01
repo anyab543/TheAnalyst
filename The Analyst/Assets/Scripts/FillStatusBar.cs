2262acae998e9e86d718cbf14fec01a8997e232e
BrandonMukadziwashe <86901969+BrandonMukadziwashe@users.noreply.github.com>
2023-04-01 13:11:07 -0400

    Health Bar

--- The Analyst/Assets/Scripts/FillStatusBar.cs
+++ The Analyst/Assets/Scripts/FillStatusBar.cs
@@ -4,41 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-// Assumes Player has a Player Health Script with Current Health and Max Health variables
+// // Assumes Player has a Player Health Script with Current Health and Max Health variables
 
 
 public class FillStatusBar : MonoBehaviour
 {
     
-    // //public PlayerHealth playerHealth;
-    // public Image fillImage;
-    // private Slider slider;
+//     // public PlayerHealth playerHealth;
+    public Image fillImage;
+    private Slider slider;
     
-    // // Start is called before the first frame update
-    // void Awake()
-    // {
-    //     slider = GetComponent<slider>();
-    // }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (slider.value <= slider.minValue) {
-    //         fillImage.enabled = false;
-    //     }
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value <= slider.minValue) {
+            fillImage.enabled = false;
+        }
 
-    //     if (slider.value > slider.minValue && !fillImage.enabled) {
-    //         fillImage.enabled = true;
-    //     }
+        if (slider.value > slider.minValue && !fillImage.enabled) {
+            fillImage.enabled = true;
+        }
 
-    //     float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
-    //     if (fillValue <= slider.maxValue / 3) {
-    //         fillImage.color = color.white; // critical condition
-    //     } else {
-    //         fillImage.color = color.red;
-    //     }
+        // float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
+
+        float fillValue = 1;
+        // if (fillValue <= slider.maxValue / 3) {
+        //     fillImage.color = color.white; // critical condition
+        // } else {
+        //     fillImage.color = color.red;
+        // }
 
 
-    //     slider.value = fillValue;
-    // }
+        slider.value = fillValue;
+    }
 }
