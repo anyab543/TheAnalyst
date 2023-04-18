ebe00c37b33dea2a2e9756296c4d9a3361de31cf
BrandonMukadziwashe <86901969+BrandonMukadziwashe@users.noreply.github.com>
2023-04-17 21:18:18 -0400

    Game Handler Script

---
+++ The Analyst/Assets/Scripts/GameHandlerScript.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class GameHandler : MonoBehaviour {
+
+        public static int playerStat1;
+
+        public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+
+        void Awake(){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+
+        void Start(){
+                pauseMenuUI.SetActive(false);
+                GameisPaused = false;
+        }
+
+        void Update(){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){ Resume(); }
+                        else{ Pause(); }
+                }
+                // Stat tester:
+                //if (Input.GetKey("p")){
+                //       Debug.Log("Player Stat = " + playerStat1);
+                //}
+        }
+
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
+
+
+        public void StartGame(){
+                SceneManager.LoadScene("Scene1");
+        }
+
+        public void OpenCredits(){
+                SceneManager.LoadScene("Credits");
+        }
+
+        public void RestartGame(){
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("MainMenu");
+                // Please also reset all static variables here, for new games!
+        }
+
+        public void QuitGame(){
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+        }
+}
