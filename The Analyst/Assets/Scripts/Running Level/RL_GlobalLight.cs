f0a12c4c92ab8463d8ed0b7be90cbd27c1d8501b
BrandonMukadziwashe <86901969+BrandonMukadziwashe@users.noreply.github.com>
2023-04-28 14:44:50 -0400

    running level lighting

---
+++ The Analyst/Assets/Scripts/Running Level/RL_GlobalLight.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RL_GlobalLight : MonoBehaviour{
+ 
+    public UnityEngine.Rendering.Universal.Light2D myGlobalLight;
+ 
+    void Start(){
+        myGlobalLight = gameObject.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        myGlobalLight.intensity = 0f;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown("e")){
+            myGlobalLight.intensity = 1f;
+            StartCoroutine(LightsDown());
+        }
+    }
+
+    IEnumerator LightsDown(){
+        yield return new WaitForSeconds(0.5f);
+        for (float i=1f; i>0f; i -= 0.01f){
+            myGlobalLight.intensity = i;
+        }
+        myGlobalLight.intensity = 0f;
+    }
+
+}
