9d927d2ec230c3dbf5f91fb08c5766f814a5cf4c
anyab543 <67363372+anyab543@users.noreply.github.com>
2023-05-02 14:12:24 -0400

    Creates space level + added object pickup scripts

---
+++ The Analyst/Assets/Scripts/SpaceLevel/Pickup.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+    public GameHandler gameHandlerObj;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        if (GameObject.FindWithTag("GameHandler") != null)
+        {
+            gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            for (int i = 0; i < inventory.slots.Length; i++){
+                if (inventory.isFull[i] == false) {
+                    /* item can be added to inventory */
+                    inventory.isFull[i] = true;
+                    /* button goes to same place as slot */
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject); /* remove picked up item */
+
+                    break;
+                }
+            }
+        }
+    }
+}
+
