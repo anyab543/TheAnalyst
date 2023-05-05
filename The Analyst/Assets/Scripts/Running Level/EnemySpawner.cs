368be511488fcd7014557a16f3e622ca110c36eb
BrandonMukadziwashe <86901969+BrandonMukadziwashe@users.noreply.github.com>
2023-05-04 20:17:35 -0400

    .

---
+++ The Analyst/Assets/Scripts/Running Level/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private float SpawnRate = 1f;
+    [SerializeField] private GameObject[] enemyPrefabs; // Array of different kind of enemies
+    [SerializeField] private bool canSpawn = true;
+
+
+    private void start() {
+        Debug.Log("In the Start function!");
+        StartCoroutine(Spawner());
+    }
+
+
+    private IEnumerator Spawner() {
+        WaitForSeconds wait = new WaitForSeconds(SpawnRate);
+
+        while (canSpawn) {
+            yield return wait;
+            int rand = Random.Range(0, enemyPrefabs.Length);
+
+            Debug.Log("In the function!");
+            
+
+            GameObject enemyToSpawn = enemyPrefabs[rand];
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+
+}
