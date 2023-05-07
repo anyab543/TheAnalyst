e44212993f806c34abdc4da79dbac01abbd0f2b1
anyab543 <67363372+anyab543@users.noreply.github.com>
2023-05-07 14:20:14 -0400

    asteroids

--- The Analyst/Assets/Scripts/SpaceLevel/deployAsteroids.cs
+++ The Analyst/Assets/Scripts/SpaceLevel/deployAsteroids.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class deployAsteroids : MonoBehaviour
 {
     public GameObject asteroids;
-    public float respawnTime = 2.0f;
-    private Vector2 screenBounds;
+    public float respawnTime = 5.0f;
+    //private Vector2 screenBounds;
+    public Transform cameraTransform;
     // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         StartCoroutine(asteroidWave());
     }
 
     private void spawn() {
             GameObject a = Instantiate(asteroids) as GameObject;
-            a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * -2);
+            a.transform.position = new Vector2(Random.Range(-(cameraTransform.position.x), cameraTransform.position.x), cameraTransform.position.y + 8);
     }
 
     IEnumerator asteroidWave() {
