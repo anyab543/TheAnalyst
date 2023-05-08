8174c7fc1911d8efbd9e7b56be48b4b2a2c759eb
vesperrr777 <chandler.bentley@tufts.edu>
2023-05-08 17:29:44 -0400

    Cut Scene Changes

---
+++ The Analyst/Assets/Scripts/DialogueSystem/Scene10aDialogue.cs
@@ -0,0 +1,167 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene10aDialogue : MonoBehaviour
+{
+    public int primeInt = 1;         // This integer drives game progress!
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    //public Text Char3name; 
+    //public Text Char3speech;
+    public GameObject DialogueDisplay;
+    public GameObject ArtChar1a;
+    //public GameObject ArtChar1b; 
+    //public GameObject ArtChar2; 
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    //public AudioSource audioSource;
+    private bool allowSpace = true;
+
+    // initial visibility settings. Any new images or buttons need to also be SetActive(false);
+    void Start()
+    {
+        DialogueDisplay.SetActive(false);
+        ArtChar1a.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    void Update()
+    {         // use spacebar as Next button
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                next();
+            }
+        }
+    }
+
+    //Story Units! The main story function. Players hit [NEXT] to progress to the next primeInt:
+    public void next()
+    {
+        primeInt = primeInt + 1;
+        if (primeInt == 1)
+        {
+            // AudioSource.Play();
+        }
+        else if (primeInt == 2)
+        {
+            ArtChar1a.SetActive(true);
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = "Who are you? Do you know what's going on?";
+        }
+        else if (primeInt == 3)
+        {
+            Char1name.text = "???";
+            Char1speech.text = "Don’t you remember me? I’m one of your patients. You’re helping me with my insomnia.";
+            Char2name.text = "";
+            Char2speech.text = "";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 4)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = ": I… I don’t remember.";
+        }
+        else if (primeInt == 5)
+        {
+            Char1name.text = "???";
+            Char1speech.text = "You remember. I’ve been having the nightmares about being lost in space. You’ve been helping me analyze my dreams.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 6)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = "I guess so. Something just seems strange. I’m really sure where to start.";
+        }
+        else if (primeInt == 7)
+        {
+            Char1name.text = "???";
+            Char1speech.text = "We can skip today’s session if you want. I’ve just been so stressed lately.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 8)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = "No no it’s okay.";
+        }
+
+
+        // after choice 1a
+        else if (primeInt == 9)
+        {
+            //gameHandler.AddPlayerStat(1); 
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = "I just feel like I’m dreaming too. Haha.";
+        }
+        else if (primeInt == 10)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Analyst";
+            Char2speech.text = "Do you know what that something is?";
+
+        }
+
+        // after choice 1b
+        else if (primeInt == 11)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "???";
+            Char2speech.text = "Haha well its like you say…";
+        }
+
+        else if (primeInt == 12)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "???";
+            Char2speech.text = "The physical and psychical may be closer than what it appears to be.";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene2Button.SetActive(true);
+        }
+
+        //Please do NOT delete this final bracket that ends the next() function: 
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+
+    public void SceneChange1()
+    {
+        SceneManager.LoadScene("Scene10b");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Scene10b");
+    }
+}
---
+++ The Analyst/Assets/Scripts/Scene10bDialogue.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene10bDialogue : MonoBehaviour
+{
+    public int primeInt = 1;         // This integer drives game progress!
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    //public Text Char3name; 
+    //public Text Char3speech;
+    public GameObject DialogueDisplay;
+    public GameObject ArtChar1a;
+    //public GameObject ArtChar1b; 
+    //public GameObject ArtChar2; 
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    //public AudioSource audioSource;
+    private bool allowSpace = true;
+
+    // initial visibility settings. Any new images or buttons need to also be SetActive(false);
+    void Start()
+    {
+        DialogueDisplay.SetActive(false);
+        ArtChar1a.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    void Update()
+    {         // use spacebar as Next button
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                next();
+            }
+        }
+    }
+
+    //Story Units! The main story function. Players hit [NEXT] to progress to the next primeInt:
+    public void next()
+    {
+        primeInt = primeInt + 1;
+        if (primeInt == 1)
+        {
+            // AudioSource.Play();
+        }
+        else if (primeInt == 2)
+        {
+            ArtChar1a.SetActive(true);
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "Your falling. Deeper into a relaxing sleep.";
+        }
+        else if (primeInt == 3)
+        {
+            Char1name.text = "";
+            Char1speech.text = "The spiral is calling you. The deeper you fall, the more relaxed you become.";
+            Char2name.text = "";
+            Char2speech.text = "";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 4)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "";
+            Char2speech.text = "Fall into the light.";
+        }
+        else if (primeInt == 5)
+        {
+            Char1name.text = "";
+            Char1speech.text = "Reach the other side.";
+            Char2name.text = "";
+            Char2speech.text = "";
+            nextButton.SetActive(false);
+            allowSpace = true;
+            NextScene1Button.SetActive(true);
+        }
+
+        //Please do NOT delete this final bracket that ends the next() function: 
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+    public void Choice1aFunct()
+    {
+        Char1name.text = "YOU";
+        Char1speech.text = "I don't know what you're talking about!";
+        Char2name.text = "";
+        Char2speech.text = "";
+        primeInt = 19;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+    public void Choice1bFunct()
+    {
+        Char1name.text = "YOU";
+        Char1speech.text = "Sure, anything you want... just lay off the club.";
+        Char2name.text = "";
+        Char2speech.text = "";
+        primeInt = 29;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+
+    public void SceneChange1()
+    {
+        SceneManager.LoadScene("SpaceLevel");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Scene2b");
+    }
+}
