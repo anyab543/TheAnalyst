5436d5f6c94c4931a58b23618866135bad3c5770
vesperrr777 <chandler.bentley@tufts.edu>
2023-05-08 23:15:52 -0400

    Working Audio Pause

--- The Analyst/Assets/StopMusic.cs
+++ The Analyst/Assets/StopMusic.cs
@@ -1,18 +1,13 @@
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class StopMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        BGSoundScript.Instance.gameObject.GetComponent<AudioSource>().Pause();
     }
 }
